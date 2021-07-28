e73d3d9a939dbbd1fdb71377afe2f68452f8d15b
Kerk Pei Yong <peiyongkerk@gmail.com>
2021-07-28 23:48:58 +0800

    Product of array except self

--- src/ProductOfArrayExceptSelf/Solution.java
+++ src/ProductOfArrayExceptSelf/Solution.java
@@ -1,4 +1,27 @@
 package ProductOfArrayExceptSelf;
 
-public class Solution {
+// Time Complexity: O(n)
+// Link to problem: https://leetcode.com/problems/product-of-array-except-self/
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int[] ans = new int[nums.length];
+        int leftProduct = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        int rightProduct = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] *= rightProduct;
+            rightProduct *= nums[i];
+
+        }
+
+        return ans;
+
+    }
 }
